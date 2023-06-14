d301b98e5d65553723c138901bf54e242b8d4fd4
Hadrien <htramoni@enssat.fr>
2023-06-14 14:01:44 +0200

    More docs

--- src/dataLoaders/CustomDatasetRaw.py
+++ src/dataLoaders/CustomDatasetRaw.py
@@ -8,14 +8,37 @@ from torchvision import transforms
 
 class CustomDataset(Dataset[Any]):
     def __init__(self, image_paths: List[str], labels: List[int], transform: transforms.Compose = None) -> None:
+        """
+        Initialize the CustomDataset class.
+
+        Args:
+            image_paths (List[str]): List of image paths.
+            labels (List[int]): List of corresponding labels.
+            transform (transforms.Compose, optional): Data transformation to be applied to the images. Defaults to None.
+        """
         self.image_paths = image_paths
         self.labels = labels
         self.transform = transform
 
     def __len__(self) -> int:
+        """
+        Return the length of the dataset.
+
+        Returns:
+            int: Length of the dataset.
+        """
         return len(self.image_paths)
 
     def __getitem__(self, index: int) -> Tuple[Union[torch.Tensor, Image.Image], torch.Tensor]:
+        """
+        Retrieve the item at the given index from the dataset.
+
+        Args:
+            index (int): Index of the item to retrieve.
+
+        Returns:
+            Tuple[Union[torch.Tensor, Image.Image], torch.Tensor]: Tuple containing the image and its label.
+        """
         image = Image.open(self.image_paths[index])
         if self.transform:
             image = self.transform(image)
--- src/dataLoaders/CustomDatasetTransformed.py
+++ src/dataLoaders/CustomDatasetTransformed.py
@@ -2,17 +2,39 @@ from typing import Any, Tuple, List
 
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
 
 
 class CustomDataset(Dataset[Any]):
-    def __init__(self, images: List[str], labels: List[int], transform: transforms.Compose = None) -> None:
+    def __init__(self, images: List[str], labels: List[int], transform: Any = None) -> None:
+        """
+        Initialize the CustomDataset class.
+
+        Args:
+            images (List[str]): List of image paths.
+            labels (List[int]): List of corresponding labels.
+            transform (transforms.Compose, optional): Data transformation to be applied to the images. Defaults to None.
+        """
         self.images = images
         self.labels = labels
         self.transform = transform
 
     def __len__(self) -> int:
+        """
+        Return the length of the dataset.
+
+        Returns:
+            int: Length of the dataset.
+        """
         return len(self.images)
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Retrieve the item at the given index from the dataset.
+
+        Args:
+            index (int): Index of the item to retrieve.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Tuple containing the image and its label.
+        """
         return torch.load(self.images[index]), torch.tensor(self.labels[index])
